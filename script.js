483b698f210cb7b56247678e27ff5933b6a0a526
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-11-30 00:05:58 -0500

    JSX + React creates plain JS objects

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// function MainContent() {
-//   return <h1>I'm learning React!</h1>;
-// }
+const navbar = (
+  <nav>
+    <h1>website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
 
-// ReactDOM.render(
-//   <>
-//     <h3>Hi</h3>
-//     <MainContent />
-//   </>,
-//   document.getElementById("root")
-// );
-
-const h1 = document.createElement("h1");
-h1.textContent = "This is an imperitive way to program";
-h1.className = "header";
-document.getElementById("root").appendChild(h1);
+ReactDOM.render(navbar, document.getElementById("root"));
